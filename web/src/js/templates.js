9a923824d5e4703a5232ff9e4c2ba1486078423e
wrgoldstein <williamrgoldstein@gmail.com>
2017-02-14 22:06:23 -0500

    x to remove event inputs

--- web/src/js/templates.js
+++ web/src/js/templates.js
@@ -1,10 +1,11 @@
 exports.event_input = function(ix){
   return $(`
-    <div class="form-group">
+    <div class="form-group" id="form-group-events-${ix}">
       <input type="text" name="events-input-${ix}" 
              list="events" id="events-input-${ix}" 
              class="events-input form-control" value="" 
              placeholder="Event">
+      <a href="#" class="close-thik"></a>
     </div>
     `)
 }
